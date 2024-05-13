d2bb77cdf6f3e7f90be0edcc20e748c6a998fb80
Ayoubchelh1 <164335444+Ayoubchelh1@users.noreply.github.com>
2024-05-13 07:59:44 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let quien = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let accion = ['ate', 'peed', 'crushed', 'broke'];
+  let que = ['my homework', 'my phone', 'the car'];
+  let cuando = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  let indQuien = Math.floor(Math.random()*quien.length);
+  let indAccion = Math.floor(Math.random()*accion.length);
+  let indQue = Math.floor(Math.random()*que.length);
+  let indCuando = Math.floor(Math.random()*cuando.length);
+
+  let excusa = indQuien + " " + indAccion + " " + indQue + " " + indCuando;
+
+  document.getElementById("excusa").textContent = excusa;
+  console.log(excusa);
 };
