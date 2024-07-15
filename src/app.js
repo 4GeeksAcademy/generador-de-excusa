8d62d61b55e00527b9656095c6be09e1611f6e34
Ayoubchelh1 <164335444+Ayoubchelh1@users.noreply.github.com>
2024-07-15 08:52:20 +0000

    new changes

--- src/app.js
+++ src/app.js
@@ -1,23 +1,27 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 
-window.onload = function() {
-  let quien = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-  let accion = ['ate', 'peed', 'crushed', 'broke'];
-  let que = ['my homework', 'my phone', 'the car'];
-  let cuando = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+function generateExcuse() {
+  let whoIndex = getRandomInt(who.length);
+  let actionIndex = getRandomInt(action.length);
+  let whatIndex = getRandomInt(what.length);
+  let whenIndex = getRandomInt(when.length);
 
-  let indQuien = Math.floor(Math.random()*quien.length);
-  let indAccion = Math.floor(Math.random()*accion.length);
-  let indQue = Math.floor(Math.random()*que.length);
-  let indCuando = Math.floor(Math.random()*cuando.length);
-
-  let excusa = indQuien + " " + indAccion + " " + indQue + " " + indCuando;
-
-  document.getElementById("excusa").textContent = excusa;
-  console.log(excusa);
+  return `${who[whoIndex]} ${action[actionIndex]} ${what[whatIndex]} ${when[whenIndex]}`;
+}
+window.onload = () => {
+  let excuseElement = document.getElementById("excuse");
+  excuseElement.innerHTML = generateExcuse();
 };
